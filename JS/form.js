8c3ff5f33ecdbdacc083aae66e86ca4d665b3c9e
Sai Jeyanth <82651681+SaiJeyanth24@users.noreply.github.com>
2023-11-21 11:14:24 +0530

    Add form page

---
+++ JS/form.js
@@ -0,0 +1,20 @@
+const button=document.querySelector('button');
+const form=document.querySelector('form');
+
+const Response = () =>{
+    form.innerHTML='<h1>Thanks for taking the time to reach out. Let me get back to you :)</h1>';
+    document.querySelector('h1').style.fontSize='40px';
+    form.style.height='150px';
+}
+
+const cancelListener = () =>{
+    document.body.style.display='none';
+}
+
+button.addEventListener('click', Response);
+
+const cancel=document.querySelector('.cancel');
+
+cancel.addEventListener('click',cancelListener);
+
+
